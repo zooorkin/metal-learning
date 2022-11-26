72051a55221de56a5a069f065c0f09e8f3a28741
zooorkin <zooorkin@icloud.com>
2022-11-26 18:09:24 +0300

    Initial Commit

---
+++ MetalLearning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+// ViewController.swift
+// MetalLearning
+//
+// Created by zooorkin on 26.11.2022
+// Copyright © 2022 zooorkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
